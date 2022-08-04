1e352b16d3bf291d3d415c54b4d56b429a1fb473
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-08-04 15:16:26 +0530

    Second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/globals.css';
-import { ChainId, ThirdWebProvider } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdWebProvider>
+    <ThirdwebProvider
+      desiredChainId={ChainId.Goerli}
+      chainRpc={{
+        [ChainId.Rinkeby]: process.env.ALCHEMY_GOERLI_RPC
+      }}
+    >
       <Component {...pageProps} />
-    </ThirdWebProvider>
+    </ThirdwebProvider>
   )
 }
 
